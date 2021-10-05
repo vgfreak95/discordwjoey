5c742a616fb87c6b60fcbfa30b4de23e66845cde
richardschroeders <towerterror00@gmail.com>
2021-10-05 01:47:24 -0400

    discord bot working!

--- jdisc.py
+++ jdisc.py
@@ -1,12 +1,26 @@
 import discord
 import json as js
 
+PREFIX = "$mc"
 
 with open("token.json", "r") as js_file:
     creds = js.load(js_file)
 
-x = 5
-print(creds['key'])
-print(type(creds))
+token = creds['token']
 
+client = discord.Client()
 
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(msg):
+    print(msg)
+    if (msg.content.startswith(PREFIX)):
+        print("Burger")
+        await msg.channel.send("From the bot!")
+    #print(f"This is the message given: {msg}")
+    
+
+client.run(token)
